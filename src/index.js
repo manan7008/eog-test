35cdf4a046ab2b49227eaa661d1b3a27c569fda7
manan7008 <manan7008@gmail.com>
2021-02-02 12:10:44 -0500

    Few more setup files

--- src/index.js
+++ src/index.js
@@ -1,5 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { ApolloProvider } from 'react-apollo';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { SubscriptionClient } from 'subscriptions-transport-ws';
+import ApolloClient from 'apollo-client';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Create WebSocket client
+const WSClient = new SubscriptionClient(`wss://react.eogresources.com/graphql`, {
+  reconnect: true,
+  connectionParams: {
+    // Connection parameters to pass some validations
+    // on server side during first handshake
+  },
+});
+
+const GraphQLClient = new ApolloClient({
+  link: WSClient,
+  cache: new InMemoryCache(),
+});
+
+ReactDOM.render(
+  <ApolloProvider client={GraphQLClient}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root'),
+);
